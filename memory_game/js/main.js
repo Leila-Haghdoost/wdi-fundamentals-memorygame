0767c9d5378fb5b473c4347ce82aff7822978a81
Leila <Leila@192-168-1-8.tpgi.com.au>
2018-09-06 15:37:59 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+var cards = ["queen", "queen", "king", "king"];
+
+cardsInPlay = [];
+
+var cardOne = [0];
+cardsInPlay.push("cardOne");
+console.log("User flipped queen");
+
+var cardTwo = [2]
+cardsInPlay.push("cardTwo");
+console.log("User flipped king")
+
+if ([0] === [2]){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+
+
