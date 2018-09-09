c478ef4c18c4c53a6a42788f8599c67656556ecc
Leila <Leila@192-168-1-8.tpgi.com.au>
2018-09-09 11:02:45 +1000

    adding event listener

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,7 +25,6 @@ cardImage: "images/king-of-diamonds.png",
 cardsInPlay = [];
 
 var checkForMatch = function () {
-
 if (cardsInPlay[0] === cardsInPlay[1]) {
 alert("You found a match!");
 } else {
@@ -34,18 +33,29 @@ alert("Sorry, try again.");
 };
 
 
-var flipCard = function (cardId) {
-	
+var flipCard = function () {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
 	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].rank)
-   checkForMatch();
+	cardsInPlay.push(cards[cardId].rank);
+  checkForMatch();
    console.log(cards[cardId].cardImage);
    console.log(cards[cardId].suit);
 };
 
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    console.log(cardElement);
+}
+};
+createBoard();
+
 
 
 
